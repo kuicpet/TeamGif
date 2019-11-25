97aa7da5ee782cfaef4d6e605d9b1a714f1c0210
Kingsley <kingsleyumujeyan@gmail.com>
2019-11-25 13:31:14 +0100

    React app created and connected to server

--- server/routes.js
+++ server/routes.js
@@ -437,5 +437,9 @@ router.get('/gifs/gitId/test', (req, res) => {
   res.status(400).send({ message: 'This is an error response' });
 });
 
+router.get('/', (req, res) => res.status(200).send({
+  status: 'success',
+  message: 'Welcome express app running',
+}));
 
 module.exports = router;
